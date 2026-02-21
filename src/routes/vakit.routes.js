c824f4a185540d2555962ee6f92b98f8805195f4
sandvic <realsandvic@gmail.com>
2026-02-21 20:00:02 +0300

    .

--- src/routes/vakit.routes.js
+++ src/routes/vakit.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { getPrayerTimes } = require("../services/prayer.service");
 const { isRamadan } = require("../services/ramadan.service");
 const { getCityFromIP } = require("../services/location.service");
-const { calculateCountdown } = require("../utils/countdown");
 
 router.get("/vakitler", async (req, res) => {
   try {
@@ -21,24 +20,17 @@ router.get("/vakitler", async (req, res) => {
       console.log("IP'den gelen şehir:", sehir);
     }
 
-    console.log("getPrayerTimes tipi:", typeof getPrayerTimes);
-
     const timings = await getPrayerTimes(sehir);
-
     console.log("TIMINGS RESULT:", timings);
 
     const ramazanMi = await isRamadan();
     console.log("Ramazan mı:", ramazanMi);
 
-    const geriSayim = calculateCountdown(timings.Maghrib);
-    console.log("Geri sayım:", geriSayim);
-
     res.json({
       sehir,
       ramazan: ramazanMi,
       imsak: timings.Fajr,
-      iftar: timings.Maghrib,
-      geriSayim
+      iftar: timings.Maghrib
     });
 
   } catch (err) {
